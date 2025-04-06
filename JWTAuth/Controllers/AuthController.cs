814076f015b016dca443f4e464bd97946b28cebe
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2025-04-06 21:13:31 +0500

    Add project files.

---
+++ JWTAuth/Controllers/AuthController.cs
@@ -0,0 +1,99 @@
+﻿using JWTAuth.Data;
+using JWTAuth.Entities;
+using JWTAuth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController: ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly DataBaseHelper dataBaseHelper;
+
+        public AuthController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+            dataBaseHelper = new DataBaseHelper(_configuration);
+        }
+
+        public static User user = new();
+
+        [Route("api/[controller]/User")]
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            var newUser = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = request.Username,
+                PasswordHash = hashedPassword
+            };
+            if(!dataBaseHelper.AddUser(newUser, out string errorMessage))
+            {
+                return BadRequest(errorMessage);
+            }
+            else
+            {
+                return Ok(newUser);
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult<User> get()
+        {
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            var user = dataBaseHelper.GetUserNameByUsername(request.Username);
+            if (user == null)
+            {
+                return BadRequest("User Not Found");
+            }
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration.GetValue<string>("AppSettings:Token")!)
+                );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
---
+++ JWTAuth/Data/DataBaseHelper.cs
@@ -0,0 +1,89 @@
+﻿using JWTAuth.Entities;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace JWTAuth.Data
+{
+    public class DataBaseHelper
+    {
+        private readonly string connectionString;
+
+        public DataBaseHelper(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConenction");
+        }
+
+        //To Register the User
+        public bool AddUser(User user, out string errorMessage)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                 con.Open();
+                int count;
+                string checkQuery = "Select count(*) from Users where Username = @Username";
+
+                using (SqlCommand checkCmd = new SqlCommand(checkQuery, con))
+                {
+                    checkCmd.Parameters.AddWithValue("@Username", user.Username);
+                    count = (int)checkCmd.ExecuteScalar();
+                }
+                if (count > 0)
+                {
+                    errorMessage = "Username Already exists";
+                    return false;
+                }
+                else
+                {
+                    string query = "Insert Into Users (Id, Username, PasswordHash) Values (@Id, @Username, @PasswordHash)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Id", user.Id);
+                        cmd.Parameters.AddWithValue("@Username", user.Username);
+                        cmd.Parameters.AddWithValue("@PasswordHash", user.PasswordHash);
+                        int rowsAffected =  cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            errorMessage = null;
+                            return true;
+                        }
+                        else
+                        {
+                            errorMessage = "Failed To Register the User.";
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        //TO check the username
+        public User? GetUserNameByUsername(string username) {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Select Id, Username, PasswordHash From Users Where Username = @Username ";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new User
+                            {
+                                Id = reader.GetGuid(0),
+                                Username = reader.GetString(1),
+                                PasswordHash = reader.GetString(2)
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
